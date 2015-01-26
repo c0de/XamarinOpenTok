907bec09a8f4fe97ced91a4ff86f72294c06df89
Jason Gotti <jgotti@marketstar.com>
2015-01-26 13:02:28 -0700

    testing for opentok

--- OpenTokAndroid/OpenTokAndroid/MainActivity.cs
+++ OpenTokAndroid/OpenTokAndroid/MainActivity.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using Android.App;
 using Android.Content;
 using Android.Runtime;
@@ -55,22 +56,48 @@ namespace OpenTokAndroid
 
 		public void OnConnected (Session p0)
 		{
+			if (mPublisher == null) {
+				mPublisher = new Publisher(this, "publisher");
+				mPublisher.SetPublisherListener(this);
+				AttachPublisherView(mPublisher);
+				mSession.Publish(mPublisher);
+			}
 		}
 
 		public void OnDisconnected (Session p0)
 		{
+			if (mPublisher != null) {
+				mPublisherViewContainer.RemoveView(mPublisher.View);
+			}
+
+			if (mSubscriber != null) {
+				mSubscriberViewContainer.RemoveView(mSubscriber.View);
+			}
+
+			mPublisher = null;
+			mSubscriber = null;
+			mStreams.Clear();
+			mSession = null;
 		}
 
-		public void OnError (Session p0, OpentokError p1)
+		public void OnError (Session session, OpentokError error)
 		{
+			// error
 		}
 
-		public void OnStreamDropped (Session p0, Stream p1)
+		public void OnStreamDropped (Session session, Stream stream)
 		{
+			if (mSubscriber != null) {
+				UnsubscribeFromStream(stream);
+			}
 		}
 
-		public void OnStreamReceived (Session p0, Stream p1)
+		public void OnStreamReceived (Session session, Stream stream)
 		{
+			mStreams.Add(stream);
+			if (mSubscriber == null) {
+				SubscribeToStream(stream);
+			}
 		}
 
 		#endregion ISessionListener
@@ -78,16 +105,22 @@ namespace OpenTokAndroid
 
 		#region IPublisherListener
 
-		public void OnError (PublisherKit p0, OpentokError p1)
+		public void OnError (PublisherKit publisher, OpentokError exception)
 		{
+			// error
 		}
 
-		public void OnStreamCreated (PublisherKit p0, Stream p1)
+		public void OnStreamCreated (PublisherKit publisher, Stream stream)
 		{
+			mStreams.Add(stream);
+			if (mSubscriber == null) {
+				SubscribeToStream(stream);
+			}
 		}
 
-		public void OnStreamDestroyed (PublisherKit p0, Stream p1)
+		public void OnStreamDestroyed (PublisherKit publisher, Stream stream)
 		{
+			UnsubscribeFromStream (stream);
 		}
 
 		#endregion IPublisherListener
@@ -95,28 +128,76 @@ namespace OpenTokAndroid
 
 		#region IVideoListener
 
-		public void OnVideoDataReceived (SubscriberKit p0)
+		public void OnVideoDataReceived (SubscriberKit subscriber)
 		{
+			AttachSubscriberView(mSubscriber);
 		}
 
-		public void OnVideoDisableWarning (SubscriberKit p0)
+		public void OnVideoDisableWarning (SubscriberKit subscriber)
 		{
+			// warning
 		}
 
-		public void OnVideoDisableWarningLifted (SubscriberKit p0)
+		public void OnVideoDisableWarningLifted (SubscriberKit subscriber)
 		{
+			// warning
 		}
 
-		public void OnVideoDisabled (SubscriberKit p0, string p1)
+		public void OnVideoDisabled (SubscriberKit subscriber, string reason)
 		{
+			// video disabled
 		}
 
-		public void OnVideoEnabled (SubscriberKit p0, string p1)
+		public void OnVideoEnabled (SubscriberKit subscriber, string reason)
 		{
+			// video enabled
 		}
 
 		#endregion IVideoListener
+
+		/**
+     * Converts dp to real pixels, according to the screen density.
+     *
+     * @param dp A number of density-independent pixels.
+     * @return The equivalent number of real pixels.
+     */
+		private int DpToPx(int dp) {
+			double screenDensity = this.Resources.DisplayMetrics.Density;
+			return (int) (screenDensity * (double) dp);
+		}
+
+		private void AttachSubscriberView(Subscriber subscriber) {
+			RelativeLayout.LayoutParams layoutParams = new RelativeLayout.LayoutParams (Resources.DisplayMetrics.WidthPixels, Resources.DisplayMetrics.HeightPixels);
+			mSubscriberViewContainer.RemoveView(mSubscriber.View);
+			mSubscriberViewContainer.AddView(mSubscriber.View, layoutParams);
+			subscriber.SetStyle(BaseVideoRenderer.StyleVideoScale, BaseVideoRenderer.StyleVideoFill);
+		}
+
+		private void AttachPublisherView(Publisher publisher) {
+			mPublisher.SetStyle(BaseVideoRenderer.StyleVideoScale, BaseVideoRenderer.StyleVideoFill);
+			RelativeLayout.LayoutParams layoutParams = new RelativeLayout.LayoutParams(320, 240);
+			layoutParams.AddRule(LayoutRules.AlignParentBottom, (int)LayoutRules.True);
+			layoutParams.AddRule(LayoutRules.AlignParentRight, (int)LayoutRules.True);
+			layoutParams.BottomMargin = DpToPx(8);
+			layoutParams.RightMargin = DpToPx(8);
+			mPublisherViewContainer.AddView(mPublisher.View, layoutParams);
+		}
+
+		private void UnsubscribeFromStream(Stream stream) {
+			mStreams.Remove(stream);
+			if (mSubscriber.Stream.Equals(stream)) {
+				mSubscriberViewContainer.RemoveView(mSubscriber.View);
+				mSubscriber = null;
+				if (mStreams.Count > 0) {
+					SubscribeToStream(mStreams.First());
+				}
+			}
+		}
+
+		private void SubscribeToStream(Stream stream) {
+			mSubscriber = new Subscriber(this, stream);
+			mSubscriber.SetVideoListener(this);
+			mSession.Subscribe(mSubscriber);
+		}
 	}
 }
-
-
